454952207a4a449eaaee518e92b0b39cd1163488
Franklin <franklin.op@hotmail.com>
2024-03-15 22:33:27 +0100

    addding option type to avoid no pointer exceptions

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
+    let get = Method::GET;
+    let delete = Method::DELETE;
+    let post = Method::POST;
+    let put = Method::PUT;
 
     let server= Server::new("127.0.0.1:8080".to_string());
     server.run();
@@ -19,4 +23,22 @@ impl Server{
         println!("Listening on {}", self.addr)
 
     }
+}
+
+struct Request{
+    path: String,
+    query_string: Option<String>,
+    method: Method,
+}
+
+enum Method{
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
 }
