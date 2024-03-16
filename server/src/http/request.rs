1289047f496a79f9ea1a198a601072a8656d702c
Franklin <franklin.op@hotmail.com>
2024-03-16 19:34:17 +0100

    converter result option to result

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -1,7 +1,9 @@
+use std::str::Utf8Error;
 use super::method::Method;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Result as FmtResult,Display, Formatter, Debug };
+use std::str;
 
 
 pub struct Request{
@@ -16,10 +18,27 @@ impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
 
     //GET /search?name=abc&sort=1 HTTP/1.1
-    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+       let request = str::from_utf8(buf)?;
+       let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+       let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+       if protocol != "HTTP/1.1" {
+        return Err(ParseError::InvalidProtocol);
+       }
+
         unimplemented!()
     }
 }
+
+    fn get_next_word(request: &str)-> Option<(&str,&str)>{
+      for (i,c) in request.chars().enumerate(){
+        if c == ' ' || c == '\r' {
+            return Some((&request [..i], &request[i + 1..]));
+        }
+      }
+      None
+    }
 
 
 
@@ -40,6 +59,12 @@ impl ParseError {
             Self::InvalidMethod =>  "Invalid Method",
 
         }
+    }
+}
+
+impl From <Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
     }
 }
 
