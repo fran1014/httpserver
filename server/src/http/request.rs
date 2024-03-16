870f597a2600a433d7702f3710c1bbcb08907170
Franklin <franklin.op@hotmail.com>
2024-03-16 15:27:16 +0100

    created custom error functionality and parse responses

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -1,4 +1,7 @@
 use super::method::Method;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Result as FmtResult,Display, Formatter, Debug };
 
 
 pub struct Request{
@@ -6,3 +9,50 @@ pub struct Request{
     query_string: Option<String>,
     method: Method,
 }
+
+
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+
+    //GET /search?name=abc&sort=1 HTTP/1.1
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!()
+    }
+}
+
+
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+    
+}
+
+impl ParseError {
+    fn message(&self)-> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod =>  "Invalid Method",
+
+        }
+    }
+}
+
+impl Display for ParseError { 
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError { 
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError {}
